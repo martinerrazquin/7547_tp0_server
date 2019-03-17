3beefbbfbdd82db4544ad36d40682562a57d0303
Martin Errazquin <martin.errazquin@hotmail.com>
2019-03-17 16:24:32 -0300

    dummy test with mocha

---
+++ test/test_books.js
@@ -0,0 +1,10 @@
+//var buildResponse = require()
+var assert = require('assert');
+
+describe('buildResponse', function () {
+  describe('succesful response', function () {
+    it('should have result field with result value', function () {
+      assert.equal(1,1);
+    })
+  })
+});
