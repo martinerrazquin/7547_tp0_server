2d0f4a696dba5bd914f48f5605bd455d19c43c2b
Rodri Zapico <rodri.zapico@gmail.com>
2019-03-17 02:37:07 -0300

    added pagination and error msg when no query is specified to /books/search endpoint

--- routes/books.js
+++ routes/books.js
@@ -15,7 +15,19 @@ module.exports = function(app, options) {
   var router = app.express.Router();
 
   router.get('/search', function(req, res, next) {
-    options.booksService.search(req.query.query, function(err, results) {
+    var defaultQuery = {page: 1};
+    var query = {...defaultQuery, ...req.query};
+
+    if (!query.query) {
+      res.status(400).json({
+        error: "NO_QUERY"
+      });
+      return;
+    }
+
+    options.booksService.search(query.query, {
+      offset: (query.page - 1) * 10
+    }, function(err, results) {
       res.json(buildResponse(err, results));
     });
   });
